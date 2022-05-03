81bacffbbdeaf405fb6cb99cab016ce31b3bc409
aussieklutz <aussieklutz@gmail.com>
2022-05-04 07:01:28 +1000

    Update AusCert.cs

--- AusCert.cs
+++ AusCert.cs
@@ -1,32 +1,33 @@
 using System.Windows.Forms;
 
 namespace AusCert
-	{
+{
 	public class Instrumentation
-		{
+	{
 		public static void Test()
-			{
+		{
 			MessageBox.Show("Test() Invoked", "AusCert");
-			}
+		}
 
 
 		public Instrumentation()
-			{
-			}
+		{
+			MessageBox.Show("Instrumentation Instantiated", "AusCert");
+		}
 
 		public void Test2()
-			{
+		{
 			MessageBox.Show("Test2() Invoked", "AusCert");
-			}
+		}
 
 		public void Invoke()
-			{
+		{
 			Instrumentation.Test();
 
 			Instrumentation i = new Instrumentation();
 			i.Test2();
 			i.Test2();
-			}
-
 		}
+
 	}
+}
